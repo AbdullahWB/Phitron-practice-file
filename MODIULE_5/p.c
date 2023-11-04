abc050d584034b6a64339635c6e2097f3a5b7bc6
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-11-04 20:49:35 +0800

    p.c

---
+++ MODIULE_5/p.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    int div=x/1000;
+    if(div%2 == 0){
+        printf("EVEN\n");
+    }else{
+        printf("ODD\n");
+    }
+    return 0;
+}
