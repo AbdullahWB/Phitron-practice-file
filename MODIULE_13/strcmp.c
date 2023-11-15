6e60e8d4d91ef02394eb822675d1a7d15ae02c9a
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-11-16 00:48:53 +0800

    strcmp

---
+++ MODIULE_13/strcmp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+    // int i = 0;
+    int v= strcmp(a, b);
+    printf("%d", v);
+    return 0;
+}
