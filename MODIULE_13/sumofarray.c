add0f5833c86730ffed7d4e9188e9884c36eb7b7
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-11-18 01:19:14 +0800

    solve problem dif

--- MODIULE_13/sumofarray.c
+++ MODIULE_13/sumofarray.c
@@ -4,10 +4,11 @@ int main()
 {
     char a[200], b[200];
     scanf("%s %s", a, b);
-    int k=strlen(a);
-    for(int i=0; i<=strlen(b); i++){
-        // printf("%d", i);
-        a[k]=b[i];
+    // strcat(a,b);
+    int k = strlen(a);
+    for (int i = 0; i <= strlen(b); i++)
+    {
+        a[k] = b[i];
         k++;
     }
     printf("%s %s", a, b);
