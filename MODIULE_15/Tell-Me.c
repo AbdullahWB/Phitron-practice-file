cb8ab6662c686ff3a4846935d5ce483be3e8a5e6
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-11-25 01:33:05 +0800

    tell me

--- MODIULE_15/Tell-Me.c
+++ MODIULE_15/Tell-Me.c
@@ -1,35 +1,57 @@
+// #include <stdio.h>
+// int main() {
+//     int T;
+//     scanf("%d", &T);
+//     while (T--) {
+//         int N;
+//         scanf("%d", &N);
+//         int A[N];
+//         for (int i = 0; i < N; i++) {
+//             scanf("%d", &A[i]);
+//         }
+//         int X;
+//         scanf("%d", &X);
+//         int isXPresent = 0;
+//         for (int i = 0; i < N; i++) {
+//             if (A[i] == X) {
+//                 isXPresent = 1;
+//                 break;
+//             }
+//         }
+//         if (isXPresent) {
+//             printf("YES\n");
+//         } else {
+//             printf("NO\n");
+//         }
+//     }
+//     return 0;
+// }
 #include <stdio.h>
-
-int main() {
-    int T;
-    scanf("%d", &T);
-
-    while (T--) {
-        int N;
-        scanf("%d", &N);
-
-        int X;
-        scanf("%d", &X);
-
-        int isXPresent = 0; // Flag to track if X is present
-
-        for (int i = 0; i < N; i++) {
-            int element;
-            scanf("%d", &element);
-
-            if (element == X) {
-                isXPresent = 1;
-                break; // Exit the loop early if X is found
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    while(a--){
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for(int i=0; i<n; i++){
+            scanf( "%d", &a[i]);
+        }
+        int x;
+        scanf("%d", &x);
+        int present = 0;
+        for(int i=0; i<n;i++){
+            if(a[i]==x){
+                present=1;
+                break;
             }
         }
-
-        // Output the result for the current test case
-        if (isXPresent) {
+        if(present){
             printf("YES\n");
-        } else {
+        }else{
             printf("NO\n");
         }
     }
-
     return 0;
 }
