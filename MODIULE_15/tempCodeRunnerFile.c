0ec6a6f028bf3e44559dfed921398552e6c1c1aa
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-11-27 01:56:17 +0800

    pattern

--- MODIULE_15/tempCodeRunnerFile.c
+++ MODIULE_15/tempCodeRunnerFile.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-// int main() {
-//     char S[100001];
-//     scanf("%s", S);
-//     int countConsonants = 0;
-//     for (int i = 0; S[i] != '\0'; i++) {
-//         switch(S[i]) {
-//             case 'a':
-//             case 'e':
-//             case 'i':
-//             case 'o':
-//             case 'u':
-//                 break;
-//             default:
-//                 countConsonants++;
-//         }
-//     }
-//     printf("%d\n", countConsonants);
-//     return 0;
-// }
+int main() {
+    char S[100001];
+    scanf("%s", S);
+    int countConsonants = 0;
+    for (int i = 0; S[i] != '\0'; i++) {
+        switch(S[i]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                break;
+            default:
+                countConsonants++;
+        }
+    }
+    printf("%d\n", countConsonants);
+    return 0;
+}
