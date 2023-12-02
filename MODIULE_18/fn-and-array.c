aef53e3f8fe5c5c05e6df93b17d3b994702ed681
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-03 01:52:44 +0800

    fn-and-array

--- MODIULE_18/fn-and-array.c
+++ MODIULE_18/fn-and-array.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+void fun (int ar[], int n){
+    // int sz=sizeof(ar)/sizeof(int);
+    // printf("%d\n",sz);
+    for(int i=0; i<n; i++){
+        printf("%d ",ar[i]);
+    }
+}
+
 int main()
 {
     int ar[5]={1,2,3,4,5};
-    int sz=sizeof(ar)/sizeof(int);
-    printf("%d",sz);
+    fun(ar, 5);
     return 0;
 }
