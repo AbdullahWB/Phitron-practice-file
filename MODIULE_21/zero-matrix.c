8993413f0acf8b01f78e47d6e2d6b1cd2698b2ff
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-09 01:10:14 +0800

    zero matrix

---
+++ MODIULE_21/zero-matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int row,col;
+    scanf("%d %d", &row, &col);
+    int a[row][col];
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int element= row*col;
+    int count = 0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(a[i][j]==0){
+                count++;
+            }
+        }
+    }
+    // printf("%d\n", count);
+    if(element == count){
+        printf("Zero Matrix");
+    }else{
+        printf("Not Zero Matrix");
+    }
+    return 0;
+}
