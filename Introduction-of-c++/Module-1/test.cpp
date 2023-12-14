1f4e3c6603924dc271c7c3754cac8015dbe08d44
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-15 01:41:08 +0800

    test.cpp

---
+++ Introduction-of-c++/Module-1/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// using namespace std;
+int main()
+{
+    // cout<< "Hello World!";
+    int a = 10;
+    long long int b = 10000000;
+    char c = 'hello';
+    float f = 1.01;
+    double d = 1.4353456;
+
+    std::cout << "hello world!" << std::endl;
+
+    std::cout << "Hello World!"
+              << "" << a << " " << b << " " << c << " " << f << " " << f << " " << d << std::endl;
+
+    return 0;
+}
