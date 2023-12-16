4092a6dbd861831577c8ebb84106777576e10ff0
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-17 00:18:12 +0800

    condition.cpp

--- Introduction-of-c++/Module-1/Condition.cpp
+++ Introduction-of-c++/Module-1/Condition.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int x;
     cin >> x;
 
-    if(x==10){
-        cout<< "It is ten out of ten" << endl;
-    }else{
-        cout<< "It is not ten out of ten" << endl;
-    }
+    // if(x==10){
+    //     cout<< "It is ten out of ten" << endl;
+    // }else{
+    //     cout<< "It is not ten out of ten" << endl;
+    // }
+
+    // ternary operator
+    (x % 2 == 0) ? cout << "Even" << endl : cout << "Odd" << endl;
 }
