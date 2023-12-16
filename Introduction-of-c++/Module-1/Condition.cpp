d26856ed6390a9061da10a8059314100da347e00
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-16 23:22:51 +0800

    condition.cpp

---
+++ Introduction-of-c++/Module-1/Condition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+
+    if(x==10){
+        cout<< "It is ten out of ten" << endl;
+    }else{
+        cout<< "It is not ten out of ten" << endl;
+    }
+}
