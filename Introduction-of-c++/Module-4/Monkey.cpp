974ef7fe302042647635b3a21f293202dd0db6e6
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-22 02:43:04 +0800

    monkey

---
+++ Introduction-of-c++/Module-4/Monkey.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[100005];
+    while(cin.getline(s, 100005)){
+        sort(s, s+strlen(s));
+        for(int i = 0; i <strlen(s); i++){
+            if(s[i]!=' '){
+                cout<<s[i];
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
