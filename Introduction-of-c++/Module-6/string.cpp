be9d8b5f285266dd3dac987ed8e5aeaacf9dc396
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-24 02:04:33 +0800

    string.cpp

--- Introduction-of-c++/Module-6/string.cpp
+++ Introduction-of-c++/Module-6/string.cpp
@@ -4,7 +4,17 @@ using namespace std;
 int main()
 {
     string a = "Hello world";
-    string a = "Hello world Gelo world";
-    cout << a << endl;
+    // string a = "Hello world Gelo world";
+    // cout << a << endl;
+
+
+    string b = "Hello world";
+
+    if(a==b){
+        cout<< "Equal" << endl;
+    }else{
+        cout<< "Not Equal" << endl;
+    }
+    
     return 0;
 }
