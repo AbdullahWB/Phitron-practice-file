fb9d0e4d05f38aff51d9aa2f6ad2420da5cbc17d
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-26 18:05:05 +0800

    string-constructor

---
+++ Introduction-of-c++/Module-7/string-constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s="Hello world!";
+    // cout << s << endl;
+    // string s("Hello world!");
+    // cout << s << endl;
+    // string s("Hello world!", 7);
+    // string a="Hello world!";
+    // string s(a,4);
+    string s(26, 'x');
+    cout << s << endl;
+
+    return 0;
+}
