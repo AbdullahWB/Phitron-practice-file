c7f3650188936b5975c46e181ce7ca3932db6ea8
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2023-12-31 00:54:52 +0800

    solve problem 1

--- Introduction-of-c++/Module-11/problem-1.cpp
+++ Introduction-of-c++/Module-11/problem-1.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-    // hello world
+    int n;
+    cin >> n;
+
+    while (n--){
+        string s,x;
+        cin >> s >> x;
+        size_t find = s.find(x);
+        while (find != string::npos)
+        {
+            s.replace(find, x.length(), "#");
+            find=s.find(x,find+1);
+        }
+        cout << s << endl;
+    }
     return 0;
 }
