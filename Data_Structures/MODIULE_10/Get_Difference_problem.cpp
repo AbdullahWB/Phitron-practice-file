61105af53f8c722bbf9914119462577f9d363a10
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-01-19 21:59:20 +0800

    get_difference_problem

---
+++ Data_Structures/MODIULE_10/Get_Difference_problem.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    };
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int find_difference(Node *head)
+{
+    int mx = INT_MIN;
+    int mn = INT_MAX;
+
+    Node *tmp = head;
+
+    while (tmp != NULL)
+    {
+        mx = max(mx, tmp->val);
+        mn = min(mn, tmp->val);
+        tmp = tmp->next;
+    }
+    return mx - mn;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+    int diff = find_difference(head);
+    cout << diff << endl;
+    return 0;
+}
