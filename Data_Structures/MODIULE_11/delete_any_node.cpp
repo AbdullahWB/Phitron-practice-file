eeb32c98c4e7862fdd5c4ddacbf3abc84b1e891e
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-01-22 13:06:41 +0800

    delete_any_node

--- Data_Structures/MODIULE_11/delete_any_node.cpp
+++ Data_Structures/MODIULE_11/delete_any_node.cpp
@@ -15,46 +15,33 @@ public:
     };
 };
 
-void insert_at_head(Node *&head, Node *&tail, int val)
+void delete_at_position(Node *head, int pos)
 {
-    Node *newNode = new Node(val);
-    if (head == NULL)
-    {
-        head = newNode;
-        tail = newNode;
-        return;
-    }
-    newNode->next = head;
-    head->prev = newNode;
-    head = newNode;
-}
-
-void insert_at_tail(Node *&head, Node *&tail, int val)
-{
-    Node *newNode = new Node(val);
-    if (tail == NULL)
-    {
-        head = newNode;
-        tail = newNode;
-        return;
-    }
-    tail->next = newNode;
-    newNode->prev = tail;
-    tail = tail->next;
-}
-
-void insert_Node_position(Node *head, int pos, int val)
-{
-    Node *newNode = new Node(val);
     Node *tmp = head;
-    for (int i = 1; i <= pos - 1; i++)
+    for (int i = 1; i <= pos-1; i++)
     {
         tmp = tmp->next;
     }
-    newNode->next = tmp->next;
-    tmp->next = newNode;
-    newNode->next->prev = newNode;
-    newNode->prev = tmp;
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
+    delete deleteNode;
+}
+
+void delete_tail(Node *&tail)
+{
+    Node *deleteNode = tail;
+    tail = tail->prev;
+    delete deleteNode;
+    tail->next = NULL;
+}
+
+void delete_head(Node *&head)
+{
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    head->prev = NULL;
 }
 
 void print_normal(Node *head)
@@ -107,24 +94,22 @@ int main()
     b->next = c;
     c->prev = b;
 
-    int pos, val;
-    cin >> pos >> val;
+    int pos;
+    cin >> pos;
 
-    if (pos == 0)
+    if (pos >= node_size(head))
     {
-        insert_at_head(head, tail, val);
+        cout << "Invalid node" << endl;
     }
-    else if (pos == node_size(head))
+    else if (pos == 0)
     {
-        insert_at_tail(head, tail, val);
+        delete_head(head);
     }
-    else if (pos >= node_size(head))
+    else if (pos == node_size(head) - 1)
     {
-        cout << "Invalid position" << endl;
-    }
-    else
-    {
-        insert_Node_position(head, 2, 100);
+        delete_tail(tail);
+    }else{
+        delete_at_position(head, pos);
     }
 
     print_normal(head);
