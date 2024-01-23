402f284ea5fec011eb8c8b17699fc90721dd5340
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-01-23 19:45:43 +0800

    list constructor

--- Data_Structures/MODIULE_12/list_constructors.cpp
+++ Data_Structures/MODIULE_12/list_constructors.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-    
+    // list<int>l;
+    // list<int>l(10,5);
+    // cout<<l.front()<<endl;
+    // list<int>l2={1,2,3,4,5};
+    // list<int>l(l2);
+    // int a[5]={10,20,30,40,50};
+    // list<int>l(a,a+5);  
+    vector<int>v={10,20,30,40,50};
+    list<int>l(v.begin(),v.end());
+
+
+    // for(auto it=l.begin(); it != l.end(); it++){
+    //     cout<<*it<<endl;
+    // }
+
+
+    for(int val:l){
+        cout<<val<<endl;
+    }
+
     return 0;
 }
