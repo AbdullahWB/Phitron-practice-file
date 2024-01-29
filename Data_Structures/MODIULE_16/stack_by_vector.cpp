495e2eaa987e935bd14e552a49084927de2d5f54
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-01-29 17:44:23 +0800

    stack_by_vector value set

--- Data_Structures/MODIULE_16/stack_by_vector.cpp
+++ Data_Structures/MODIULE_16/stack_by_vector.cpp
@@ -5,27 +5,43 @@ class myStack
 {
 public:
     vector<int> v;
-    void push(int val){
+    void push(int val)
+    {
         v.push_back(val);
     }
-    void pop(){
+    void pop()
+    {
         v.pop_back();
-    }   
-    int top(){
+    }
+    int top()
+    {
         return v.back();
     }
-    int size(){
+    int size()
+    {
         return v.size();
     }
-    bool empty(){
-        if(v.size()==0) return true;
-        else return false; 
+    bool empty()
+    {
+        if (v.size() == 0)
+            return true;
+        else
+            return false;
     }
-}
+};
 
-int
-main()
+int main()
 {
-
+    myStack st;
+    st.push(10);
+    st.push(20);
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    if (st.empty() == false)
+    {
+        cout << st.top() << endl;
+    }
     return 0;
 }
