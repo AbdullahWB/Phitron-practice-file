8c91e4fc26c0f58257f3c2236cc99182314690fe
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-01-29 12:45:29 +0800

    Queries_Again

---
+++ Data_Structures/MODIULE_15_mid/Queries_Again.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    };
+};
+
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+}
+
+void insert_Node_position(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = tmp;
+}
+
+void print_normal(Node *head)
+{
+    cout<<"L -> ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_reverse(Node *tail)
+{
+    cout<<"R -> ";
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int node_size(Node *head)
+{
+    Node *tmp = head;
+    int cnt = 0;
+    while (tmp != NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+
+int main()
+{
+    int q;
+    cin >> q;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (q--)
+    {
+        int x, v;
+        cin >> x >> v;
+
+        if (x == 0)
+        {
+            insert_at_head(head, tail, v);
+        }
+        else if (x == node_size(head))
+        {
+            insert_at_tail(head, tail, v);
+        }
+        else if (x > node_size(head))
+        {
+            cout << "Invalid" << endl;
+            continue;
+        }
+        else
+        {
+            insert_Node_position(head, x, v);
+        }
+        print_normal(head);
+        print_reverse(tail);
+    }
+    return 0;
+}
