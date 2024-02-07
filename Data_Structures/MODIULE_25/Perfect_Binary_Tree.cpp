03643bbf3b20fd2422c4135901ed4b422120ad7e
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-02-07 18:31:44 +0800

    Perfect_Binary_Tree

---
+++ Data_Structures/MODIULE_25/Perfect_Binary_Tree.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+    {
+        root == NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1;
+        Node *f = q.front();
+        q.pop();
+
+        // 2
+        int l, r;
+        cin >> l >> r;
+        Node *left;
+        Node *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        f->left = left;
+        f->right = right;
+
+        // 3
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+    return root;
+}
+
+int count_node(Node*root){
+    if (root == NULL) return 0;
+
+    int l=count_node(root->left);
+    int r=count_node(root->right);
+
+    return l+r+1;
+}
+
+int max_height(Node *root){
+    if (root == NULL) return 0;
+    int l=max_height(root->left);
+    int r=max_height(root->right);
+    return max(l,r)+1;
+}
+
+int main()
+{
+    Node *root = input_tree();
+    int nodes=count_node(root);
+    int height = max_height(root);
+    if(nodes==(pow(2,height)-1)){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
+
