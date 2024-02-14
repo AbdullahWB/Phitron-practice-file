be309a47798d16064add8db8df3f7e69d56bb654
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-02-14 19:22:22 +0800

    Give_Current_Min

--- Data_Structures/MODIULE_30/Count_Me.cpp
+++ Data_Structures/MODIULE_30/Count_Me.cpp
@@ -6,22 +6,25 @@ int main()
     int t;
     cin >> t;
     cin.ignore();
-    while(t--){
-        map<string,int>mp;
+    while (t--)
+    {
+        map<string, int> mp;
         string sentence;
         getline(cin, sentence);
         string word;
         stringstream ss(sentence);
         string ans;
         int mx = 0;
-        while(ss >> word){
+        while (ss >> word)
+        {
             mp[word]++;
-            if(mp[word]>mx){
+            if (mp[word] > mx)
+            {
                 mx = mp[word];
-                ans=word;
+                ans = word;
             }
         }
-        cout<<ans<< " "<<mx<<endl;
+        cout << ans << " " << mx << endl;
     }
     return 0;
 }
