69d3978115022115a5309b5a7d8a5141189fedf1
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-02-16 00:19:24 +0800

    adjacency_matrix

---
+++ Introduction_Of_Algorithms/MODIULE/adjacency_matrix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int met[n][n];
+    memset(met, 0, sizeof(met));
+    while(e--){
+        int a,b;
+        cin >> a >> b;
+        met[a][b]=1;
+        met[b][a]=1;
+    }
+    for (int i = 0; i <= n - 1; i++)
+    {
+        for (int j = 0; j <= n - 1; j++)
+        {
+            cout << met[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
