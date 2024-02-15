921bb742302a7e7f0866a733940a692620b6a027
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-02-16 00:55:47 +0800

    stl_pair

---
+++ Introduction_Of_Algorithms/MODIULE/stl_pair.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<int, string>p;
+    p=make_pair(1,"Mohammad Abdullah");
+    cout<<p.first<<" "<<p.second<<endl;
+    return 0;
+}
