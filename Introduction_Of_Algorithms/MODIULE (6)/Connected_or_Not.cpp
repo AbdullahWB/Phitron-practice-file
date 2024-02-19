0434879d5cd0a2c7d176475dd1c3d86d94a9735d
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-02-19 23:46:48 +0800

    Connected_or_Not

--- Introduction_Of_Algorithms/MODIULE (6)/Connected_or_Not.cpp
+++ Introduction_Of_Algorithms/MODIULE (6)/Connected_or_Not.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main()
 {
-    
+   int n, e;
+    cin >> n >> e;
+    int met[n][n];
+    memset(met, 0, sizeof(met));
+    while(e--){
+        int a,b;
+        cin >> a >> b;
+        met[a][b]=1;
+        // met[b][a]=1; 
+    }
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        if(met[x][y] || x==y){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+
     return 0;
 }
