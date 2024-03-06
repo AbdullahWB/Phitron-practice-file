374605f3ca6e5be67cefbe165984bafec4a36219
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-06 22:01:38 +0800

    circumference

---
+++ Introduction-of-c/MODiULE_1/circumference.c
@@ -0,0 +1,40 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int circum=4;
+//     int side_length=25;
+//     int circumference=side_length*circum;
+//     printf("a side length of 25, write a program to calculate its circumference is:-> %d\n", circumference);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     /*  Input your code here              */
+
+//     int circum = 4;
+//     int side_length = 25;
+//     int circumference = side_length * circum;
+
+//     /*************************************/
+
+//     printf("%d\n", circumference);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int q=100;
+//     int sum=0;
+//     for(int i=0; i<=q;i++){
+//         sum +=i;
+//     }
+//     printf("%d\n", sum);
+
+//     return 0;
+// }
