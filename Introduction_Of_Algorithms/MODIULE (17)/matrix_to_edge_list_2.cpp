137ec441571ac7fd4f9030748e64b5c15a00be45
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-08 00:18:22 +0800

    matrix_to_edge_list_2

--- Introduction_Of_Algorithms/MODIULE (17)/matrix_to_edge_list_2.cpp
+++ Introduction_Of_Algorithms/MODIULE (17)/matrix_to_edge_list_2.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Edge
+{
+public:
+    int u, v, c;
+    Edge(int u, int v, int c)
+    {
+        this->u = u;
+        this->v = v;
+        this->c = c;
+    }
+};
+
 int main()
 {
     int n;
@@ -13,20 +25,20 @@ int main()
             cin >> mat[i][j];
         }
     }
-    vector<pair<int, int>> edgeList;
+    vector<Edge> edgeList;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (mat[i][j] == 1 && i != j)
+            if (mat[i][j] >0)
             {
-                edgeList.push_back({i, j});
+                edgeList.push_back(Edge(i, j, mat[i][j]));
             }
         }
     }
-    for (pair<int, int> edge : edgeList)
+    for (Edge edge : edgeList)
     {
-        cout << edge.first << " " << edge.second << endl;
+        cout << edge.u << " " << edge.v << " " << edge.c << endl;
     }
     return 0;
 }
