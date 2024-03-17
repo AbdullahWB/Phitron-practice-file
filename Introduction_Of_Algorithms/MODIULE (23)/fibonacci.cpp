405793cace7f8360bf7138f5b33bbce34bc08e1b
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-18 00:31:10 +0800

    fibonacci

---
+++ Introduction_Of_Algorithms/MODIULE (23)/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    int ans = fibonacci(n - 1) + fibonacci(n - 2);
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int fibonacciCnt = fibonacci(n);
+    cout << fibonacciCnt << endl;
+    return 0;
+}
