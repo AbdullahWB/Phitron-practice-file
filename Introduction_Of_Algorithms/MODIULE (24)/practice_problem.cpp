ba374f68486098e33f37e4153b981e84936a5bad
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-22 00:06:22 +0800

    practice problem

---
+++ Introduction_Of_Algorithms/MODIULE (24)/practice_problem.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/U
+    return 0;
+}
---
+++ Introduction_Of_Algorithms/MODIULE (25)/practice_problem.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // https://docs.google.com/document/d/1wOSqYZlDa_XI1JoHsJ9t9O6Ao_-cjJlr/edit
+    return 0;
+}
