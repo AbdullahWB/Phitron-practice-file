a55d207a4563e20c19dd0f3bae4a016acaedcf76
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-23 14:50:00 +0800

    count_bottom_up_sum

--- Introduction_Of_Algorithms/MODIULE (26)/count_subset_sum.cpp
+++ Introduction_Of_Algorithms/MODIULE (26)/count_subset_sum.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int dp[1005][1005];
 
-bool subset_sum(int n, int a[], int s)
+int subset_sum(int n, int a[], int s)
 {
     if (n == 0)
     {
         if (s == 0)
-            return true;
+            return 1;
         else
-            return false;
+            return 0;
     }
     if (dp[n][s] != -1)
         return dp[n][s];
     if (a[n - 1] <= s)
     {
-        bool op1 = subset_sum(n - 1, a, s - a[n - 1]);
-        bool op2 = subset_sum(n - 1, a, s);
-        return dp[n][s] = op1 || op2;
+        int op1 = subset_sum(n - 1, a, s - a[n - 1]);
+        int op2 = subset_sum(n - 1, a, s);
+        return dp[n][s] = op1 + op2;
     }
     else
     {
@@ -37,16 +37,13 @@ int main()
     }
     int s;
     cin >> s;
-     for (int i = 0; i <= n; i++)
+    for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j <= s; j++)
         {
             dp[i][j] = -1;
         }
     }
-    if (subset_sum(n, a, s))
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
+    cout << subset_sum(n, a, s) << endl;
     return 0;
 }
