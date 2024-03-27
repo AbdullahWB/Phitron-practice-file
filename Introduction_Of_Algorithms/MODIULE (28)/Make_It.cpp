51fc1c0086ce83eb387a95a5270655ce5caafac1
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-03-28 00:32:20 +0800

    make it

--- Introduction_Of_Algorithms/MODIULE (28)/Make_It.cpp
+++ Introduction_Of_Algorithms/MODIULE (28)/Make_It.cpp
@@ -1,26 +1,43 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-bool subSetSum(ll initial, ll n) {
-    if (initial == n) return true;
-    if (initial > n) return false;
+const int N = 1e5 + 5;
+int dp[N];
 
-    bool op1 = subSetSum(initial + 3, n);
-    bool op2 = subSetSum(initial * 2, n);
+bool subSetSum(int n, int target)
+{
+    if (n == target)
+        return true;
+    if (n > target)
+        return false;
+    if (dp[n] != -1)
+        return dp[n];
+    ;
 
-    return op1 || op2;
+    bool op1 = subSetSum(n + 3, target);
+    bool op2 = subSetSum(n * 2, target);
+
+    return dp[n] = op1 || op2;
 }
 
-int main() {
-    ll t;
+int main()
+{
+    int t;
     cin >> t;
-    while (t--) {
-        ll n;
+    while (t--)
+    {
+        int n;
         cin >> n;
-        if (subSetSum(1, n)) {
+        for (int i = 0; i <= n; ++i)
+        {
+            dp[i] = -1;
+        }
+        if (subSetSum(1, n))
+        {
             cout << "YES" << endl;
-        } else {
+        }
+        else
+        {
             cout << "NO" << endl;
         }
     }
--- Introduction_Of_Algorithms/MODIULE (28)/Water.cpp
+++ Introduction_Of_Algorithms/MODIULE (28)/Water.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> heights(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> heights[i];
+        }
+        
+        int left = 0, right = n - 1;
+        int maxArea = 0;
+        int leftIndex = 0, rightIndex = 0;
+        
+        while (left < right) {
+            int area = min(heights[left], heights[right]) * (right - left);
+            if (area > maxArea) {
+                maxArea = area;
+                leftIndex = left;
+                rightIndex = right;
+            }
+            if (heights[left] < heights[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        
+        cout << leftIndex << " " << rightIndex << endl;
+    }
     return 0;
 }
