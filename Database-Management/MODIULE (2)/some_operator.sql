a884f3fc03f0fc7789c4007f61dff73d7494ae42
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-04-09 17:47:09 +0800

    data and some operation

---
+++ Database-Management/MODIULE (2)/some_operator.sql
@@ -0,0 +1,17 @@
+SELECT * 
+FROM COUNTRIES;
+
+SELECT * 
+FROM DEPARTMENTS;
+
+SELECT *
+FROM EMPLOYEES
+
+-- this for select any table or column and any thing that you want to.
+
+SELECT *
+FROM EMPLOYEES
+WHERE DEPARTMENT_ID = 60 OR SALARY > 6000
+
+-- this use for conditional selection of all tables AND // OR.
+
