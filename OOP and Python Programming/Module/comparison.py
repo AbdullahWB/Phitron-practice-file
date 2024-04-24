f814631b1143fdb96ae3dfeb235fe94423192acb
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-04-24 21:57:56 +0800

    comparison

---
+++ OOP and Python Programming/Module/comparison.py
@@ -0,0 +1,37 @@
+# <, >, <=, >=, ==, !==, 
+
+# Check if two integers are equal
+x = 10
+y = 10
+if x == y:
+    print("x and y are equal")
+
+# Check if two floats are not equal
+x = 10.5
+y = 10.6
+if x != y:
+    print("x and y are not equal")
+
+# Check if one integer is greater than another
+x = 10
+y = 5
+if x > y:
+    print("x is greater than y")
+
+# Check if one float is greater than or equal to another
+x = 10.5
+y = 10.5
+if x >= y:
+    print("x is greater than or equal to y")
+
+# Check if one string is less than another
+x = "Hello"
+y = "World"
+if x < y:
+    print("x is less than y")
+
+# Check if one boolean is less than or equal to another
+x = True
+y = False
+if x <= y:
+    print("x is less than or equal to y")
---
+++ OOP and Python Programming/Module/operators.py
@@ -0,0 +1,9 @@
+num1=40
+num2=11
+
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+print(num1%num2)
+print(num1//num2)
