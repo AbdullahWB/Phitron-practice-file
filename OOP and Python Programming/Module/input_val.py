98b226b68cc12ddd625ee5cdd2fbc7aacf690808
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-04-24 15:58:29 +0800

    input-val

---
+++ OOP and Python Programming/Module/input_val.py
@@ -0,0 +1,47 @@
+# print("taka de")
+# input()
+
+# input("taka de:- ")
+
+sauda = input("I give you some gift for your birthday my heart:- ")
+ma = input("I give you some gift for your birthday ma:- ")
+total= sauda+ma
+print(type(sauda))
+print(type(ma))
+print(total) #this print is for string
+
+
+
+sauda = input("I give you some gift for your birthday my heart:- ")
+ma = input("I give you some gift for your birthday ma:- ")
+sauda_int=int(sauda)
+ma_int=int(ma)
+total= sauda_int+ma_int
+print(total) #this print is for int but its very complex
+
+
+
+sauda= int(input("I give you some gift for your birthday my heart:- "))
+ma= int(input("I give you some gift for your birthday ma:- "))
+total= sauda+ma
+print("my total taka:- ", total)
+
+
+# taka = int(input("taka de:- "))
+
+# print(type(taka))
+
+
+
+
+
+
+
+""" 
+
+    google search 
+    1. convert number to string: str
+    2. convert decimal to number: float
+    3. python int vs float
+
+"""
--- OOP and Python Programming/Module/tempCodeRunnerFile.py
+++ OOP and Python Programming/Module/tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-print("ki obostha python mama")
+sauda= int(input("I give you some gift for your birthday my heart:- "))
+ma= int(input("I give you some gift for your birthday ma:- "))
+total= sauda+ma
+print("my total taka:- ", total)
