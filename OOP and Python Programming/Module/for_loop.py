d762842449fcc5f39701cac78bda70516a6ab401
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-04-26 23:15:59 +0800

    for_loop

---
+++ OOP and Python Programming/Module/for_loop.py
@@ -0,0 +1,28 @@
+number=[4,10,15,20,25]
+sum=0
+for num in number:
+    print(num)
+    sum=sum+num
+    print("sum num", sum)
+    if sum<=num:
+        print("sum is less than num")
+
+
+text= "I love you sauda"
+for index ,char in enumerate(text): # enumerate is used to print index and char
+    print(index,char)
+    if char=="a":
+        print("I love you sauda")
+    else:
+        print("I hate you sauda")
+
+
+
+for i in range(1,11,1):
+    print("I love you sauda", i) 
+
+
+
+items=['baseball','basketball','football']
+for index, item in enumerate(items):
+    print(index, item)
--- OOP and Python Programming/Module/tempCodeRunnerFile.py
+++ OOP and Python Programming/Module/tempCodeRunnerFile.py
@@ -1,14 +1,7 @@
-numB=0
-while numB <= 10:
-    numB += 1
-    if numB % 2 == 0:
-        print("even", numB)
-    if numB % 2 == 1:
-        print("odd", numB)
-    if numB == 9:
-        print("numB is 9")
-        break
-    if numB == 5:
-        continue
-        print("numB is 5")
-    
+text= "I love you sauda"
+for index ,char in enumerate(text):
+    print(index,char)
+    if char=="a":
+        print("I love you sauda")
+    else:
+        print("I hate you sauda")
