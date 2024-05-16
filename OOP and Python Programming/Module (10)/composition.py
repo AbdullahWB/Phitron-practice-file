f6a2e23f3f5cd8dfe36317ae90139d6bfe3022d5
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-05-16 22:58:56 +0800

    star_cinema

--- OOP and Python Programming/Module (10)/composition.py
+++ OOP and Python Programming/Module (10)/composition.py
@@ -1,3 +1,42 @@
+"""
+!composition vs inheritance in python
+
+Choosing Between Inheritance and Composition in Python
+Python, as an object oriented programming language, supports both inheritance 
+and composition.
+You saw that inheritance is best used to model an is a relationship, whereas
+composition models a has a relationship.
+
+Sometimes, it's hard to see what the relationship between two classes should
+be, but you can follow these guidelines:
+
+	Use inheritance over composition in Python to model a clear is a 
+    relationship. First, justify the relationship between the derived
+    class and its base. Then, reverse the relationship and try to justify it. 
+    If you can justify the relationship in both directions, then you should
+    not use inheritance between them.
+
+	Use inheritance over composition in Python to leverage both the interface
+    and implementation of the base class.
+
+	Use inheritance over composition in Python to provide mixin features to
+    severalunrelated classes when there is only one implementation of that
+    feature.
+
+	Use composition over inheritance in Python to model a has a relationship
+    that leverages the implementation of the component class.
+
+	Use composition over inheritance in Python to create components that can
+    be reused by multiple classes in your Python applications.
+
+	Use composition over inheritance in Python to implement groups of 
+    behaviors and policies that can be applied interchangeably to other 
+    classes to customize their behavior.
+
+	Use composition over inheritance in Python to enable run-time behavior
+    changes without affecting existing classes.
+"""
+
 class CPU:
     def __init__(self, cores) -> None:
         self.cores = cores
---
+++ OOP and Python Programming/Module (12)/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+pagla_hall.book_seats("S2", [(0, 0), (0, 2)])
+pagla_hall.book_seats("S2", [(0, 0), (0, 3)])
+pagla_hall.book_seats("S2", [(0, 0), (0, 4)])
