4b7285d89819e82335bcd9f93a36beb7a63de1f8
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-06-01 13:51:10 +0800

    variable type

---
+++ Software Development Project/Module (6)/type.js
@@ -0,0 +1,14 @@
+const nam = 10
+const nam2 = "h"
+const nam3 = "hello world"
+const nam4 = true
+const nam5 = {name: "abdullah"}
+const nam6 = 10.50
+
+
+console.log(typeof(nam))
+console.log(typeof(nam2))
+console.log(typeof(nam3))
+console.log(typeof(nam4))
+console.log(typeof(nam5))
+console.log(typeof(nam6))
