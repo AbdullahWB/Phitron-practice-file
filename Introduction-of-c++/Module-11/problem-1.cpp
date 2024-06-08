ca1388a687b579448c7834b40322b35e848d5510
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-06-08 19:48:57 +0800

    project file set up

--- Introduction-of-c++/Module-11/problem-1.cpp
+++ Introduction-of-c++/Module-11/problem-1.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
 
-    while (n--){
-        string s,x;
+    while (n--) {
+        string s, x;
         cin >> s >> x;
-        size_t find = s.find(x);
-        while (find != string::npos)
-        {
-            s.replace(find, x.length(), "#");
-            find=s.find(x,find+1);
+        int pos;
+        while ((pos = s.find(x)) != -1) {
+            s.replace(pos, x.length(), "#");
         }
         cout << s << endl;
     }
+    
     return 0;
 }
---
+++ Software Development Project/Module (9)/app.js
@@ -0,0 +1,3 @@
+const target = document.getElementById("title");
+target.style.color = "rgba(5, 5, 205)"
+console.log(target);
