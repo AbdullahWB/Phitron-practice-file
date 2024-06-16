af93867b0bde55b2f90df37b36cd07d1680f7e4f
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-06-16 18:06:48 +0800

    creaat routing

--- Software Development Project/Module (12)/simple_page_navigation/navigation/views.py
+++ Software Development Project/Module (12)/simple_page_navigation/navigation/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request ,'first_app/home.html')
+    return render(request ,'navigation/main.html')
+
+def about(request):
+    return render(request ,'navigation/about.html')
+
+def contact(request):
+    return render(request ,'navigation/contact.html')
--- Software Development Project/Module (12)/simple_page_navigation/simple_page_navigation/urls.py
+++ Software Development Project/Module (12)/simple_page_navigation/simple_page_navigation/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index),
-    path("first_app/", include("first_app.urls")),
+    path("navigation/", include("navigation.urls")),
 ]
