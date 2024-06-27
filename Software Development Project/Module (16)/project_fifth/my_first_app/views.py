c118be82287aded8aed49a15bdec4e19a64a128a
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-06-28 01:31:45 +0800

    html form

--- Software Development Project/Module (16)/project_fifth/my_first_app/views.py
+++ Software Development Project/Module (16)/project_fifth/my_first_app/views.py
@@ -7,7 +7,19 @@ def index(request):
 
 
 def about(request):
-    return render(request, "./my_first_app/about.html")
+    print(request.POST)
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        select = request.POST.get("select")
+        return render(
+            request,
+            "./my_first_app/about.html",
+            {"email": email, "password": password, "select": select},
+        )
+    else:
+        return render(request, "./my_first_app/about.html")
+
 
 def submit_from(request):
     return render(request, "./my_first_app/form.html")
