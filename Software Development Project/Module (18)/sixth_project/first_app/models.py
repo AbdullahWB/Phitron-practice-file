f9c46336162c442004518bda572aed483bd70234
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-07-15 13:47:52 +0800

    show data in web page

--- Software Development Project/Module (18)/sixth_project/first_app/models.py
+++ Software Development Project/Module (18)/sixth_project/first_app/models.py
@@ -7,3 +7,6 @@ class Student(models.Model):
     roll = models.IntegerField(primary_key=True)
     address = models.TextField()
     father_name = models.TextField(null= True) 
+    
+    def __str__(self):
+        return f'Roll: {self.roll} - Name: {self.name}'
