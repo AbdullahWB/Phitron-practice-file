8d51a86d0ec54ec37dd2963a5eb10600bd1a0993
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-07-16 01:26:42 +0800

    show from data

--- Software Development Project/Module (18)/sixth_project/first_app/urls.py
+++ Software Development Project/Module (18)/sixth_project/first_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name='homePage'),
+    path("delete/<int:roll>", views.delete_student, name="delete_student"),
 ]
