73bc50ece7c2311ee88aa98574b41259ac3e1f5a
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-07-20 18:31:22 +0800

    set modiuls

--- Software Development Project/Module (19)/blog_project/author/urls.py
+++ Software Development Project/Module (19)/blog_project/author/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_author, name='add_author')
+    # path('add/', views.add_author, name='add_author')
 ]
