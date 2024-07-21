6fa420775a9edaf2be9663c3c68172b17d4215af
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-07-21 18:19:10 +0800

    show a form

--- Software Development Project/Module (19)/blog_project/author/views.py
+++ Software Development Project/Module (19)/blog_project/author/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 
 def add_author(request):
-    return render(request, 'author/index.html')
+    author_form = forms.AuthorFrom()
+    return render(request, 'author/index.html', {'form': author_form})
