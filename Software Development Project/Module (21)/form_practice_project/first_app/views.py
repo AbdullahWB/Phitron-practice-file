951db229ebf2ecf461d005cec4abf278c44a30f0
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-08-04 17:36:23 +0800

    practice form add crispy

--- Software Development Project/Module (21)/form_practice_project/first_app/views.py
+++ Software Development Project/Module (21)/form_practice_project/first_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # process form data
+            pass
+    else:
+        form = ExampleForm()
+
+    return render(request, 'home.html', {'form': form})
