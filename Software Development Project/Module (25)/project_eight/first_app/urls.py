5549a3eaa4d2b2d5d3635f15318824a77b84fc21
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-08-13 17:29:21 +0800

    login singup profile

--- Software Development Project/Module (25)/project_eight/first_app/urls.py
+++ Software Development Project/Module (25)/project_eight/first_app/urls.py
@@ -20,4 +20,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.signup, name='login'),
+    path('profile/', views.profile, name='profile'),
 ]
--- Software Development Project/Module (25)/project_eight/first_app/views.py
+++ Software Development Project/Module (25)/project_eight/first_app/views.py
@@ -1,14 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
 def home(request):
+    return render(request, "./home.html")
+
+def signup(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
+            messages.success(request, 'Account has been registered successfully')
+            form.save(commit=True)
+            print(form.cleaned_data)
     else:
         form = RegisterForm()
-    return render(request, "./home.html", {'form': form})
+    return render(request, "./signup.html", {'form': form})
+ 
 
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = name, password = password) #check if user already exists
+            if user is not None:
+                login(request, user)
+                redirect('profile')
+    else:
+        form = AuthenticationForm()
+        return render(request, "./login.html", {'form': form})
+    
+    
+def profile(request):
+    return render(request, "./profile.html")        
