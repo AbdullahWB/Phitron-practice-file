31af826bcfc3e4f36444754bd35d0c332d0fc4e7
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-08-19 18:11:48 +0800

    profile

--- Software Development Project/Module (26)/blog_project_part_2/author/urls.py
+++ Software Development Project/Module (26)/blog_project_part_2/author/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='user_login')
+    path('login/', views.user_login, name='user_login'),
+    path('profile/', views.profile, name='profile'),
+    path('pass_change/', views.pass_change, name='pass_change')
 ]
--- Software Development Project/Module (26)/blog_project_part_2/author/views.py
+++ Software Development Project/Module (26)/blog_project_part_2/author/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from . import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -37,10 +38,35 @@ def user_login(request):
             if user is not None:
                 messages.success(request, 'your account successfully log in')
                 login(request, user)
-                return redirect('user_login')
+                return redirect('profile')
             else:
                 messages.warning(request, 'log in information is not available')
                 return redirect('register')
     else:
         form = AuthenticationForm()
-    return render(request, 'author/register.html', {'form': form, 'type':'Login'}) #12
+    return render(request, 'author/register.html', {'form': form, 'type':'Login'}) 
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = forms.ChangeUserData(request.POST, instance = request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'profile account update successfully ')
+            return redirect('profile')
+    else:
+        form = forms.ChangeUserData(instance = request.user)
+    return render(request, 'author/profile.html', {'form': form}) 
+
+
+def pass_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST, request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'password updated successfully')
+            update_session_auth_hash(request, form.user)
+            return redirect('profile')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'author/pass_change.html', {'form': form})
