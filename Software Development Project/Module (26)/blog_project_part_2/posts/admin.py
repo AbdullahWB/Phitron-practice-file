0606e0cb1e2259c4183807de8b214e784e205b3d
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-08-20 21:52:25 +0800

    add slug

--- Software Development Project/Module (26)/blog_project_part_2/posts/admin.py
+++ Software Development Project/Module (26)/blog_project_part_2/posts/admin.py
@@ -3,3 +3,9 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Post)
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('title',)}
+#     list_display = ['title', 'slug',]
+
+# admin.site.register(models.Category, CategoryAdmin)
