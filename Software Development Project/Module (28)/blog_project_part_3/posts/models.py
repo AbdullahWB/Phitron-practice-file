04f6a8752a6a0c93a07653d3496f40dcb0702594
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-08-24 16:50:12 +0800

    add media file

--- Software Development Project/Module (28)/blog_project_part_3/posts/models.py
+++ Software Development Project/Module (28)/blog_project_part_3/posts/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     category = models.ManyToManyField(Category)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/media/uploads', blank=True, null=True)
     
     def __str__(self):
         return f'Title:- {self.title}'
